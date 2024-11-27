e9d6e81d383b700580be071c33871623f5b81165
= <=>
2024-11-27 16:15:26 +0100

    asdas

--- 2.- Sensores y actuadores/Ejercicio2.5/Ejercicio2.5.ino
+++ 2.- Sensores y actuadores/Ejercicio2.5/Ejercicio2.5.ino
@@ -7,145 +7,134 @@
    - Martín Cancio Barrera  UO287561
   *****************************************************************************
 */
+
 #include <Keypad.h>
 
-// pins
-int redLed = 9;
-int greenLed = 8;
-int foto = A5;
+int ledRojo = 7;
+int ledVerde = 8;
 
-int trig = 11;
-int echo = 10;
-unsigned long millisTime = 0;
-unsigned long result = 0;
+const byte filas = 4;
+const byte columnas = 4;
+char keys[filas][columnas] = {{'1','2','3','A'},
+                              {'4','5','6','B'},
+                              {'7','8','9','C'},
+                              {'*','0','#','D'}};
+byte pinesFila[filas] = {2, 3, 4, 5};
+byte pinesColumna[columnas] = {A0, A1, A2, A3};
+Keypad keypad = Keypad(makeKeymap(keys), pinesFila, pinesColumna, filas, columnas);
 
-const byte rows = 4;
-const byte cols = 4;
+unsigned long milis = 0;
+unsigned long milis5000 = 0;
+unsigned long resultTime = 0;
 
-bool open = false;
+const byte longitudPass = 4; 
+char password[longitudPass] = {'9','8','7','6'};  
 
-// Matriz mapping keypad
-char keys[rows][cols] = {
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
-};
+int trig = 10;
+int echo = 11;
 
-byte pinsRow[rows] = {2, 3, 4, 5};
-byte pinsCol[cols] = {A0, A1, A2, A3};
+int fotorresistor = A5;
+int clk7Seg = 9;
+int d10Seg = 12;
 
-// Contraseña
-char passw[4] = {'9', '8', '7', '6'};
-
-// Pines del KeyPad
-Keypad keypad = Keypad(makeKeymap(keys), pinsRow, pinsCol, rows, cols);                              
+int gente = 0;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(redLed, OUTPUT);
-  pinMode(greenLed, OUTPUT);
-  // Cerrado -> Led roja
-  digitalWrite(greenLed, LOW);
-  digitalWrite(redLed, HIGH);
-  // Ultrasound
+  pinMode(ledRojo, OUTPUT);
+  pinMode(ledVerde, OUTPUT);
+
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
+
+  pinMode(fotorresistor,INPUT);
+
+  digitalWrite(ledVerde, HIGH);
+  digitalWrite(ledRojo, LOW);
+  Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(greenLed, LOW);
-  digitalWrite(redLed, HIGH);
-  
-  if(isPassword()) {
-    open = true;
-  }
-
-  if (keypad.waitForKey() == 'C' && open) {
-    // Cerrado -> Led roja
-    digitalWrite(greenLed, LOW);
-    digitalWrite(redLed, HIGH);
-    open = false;
-  }
-}
-
-bool isPassword(){
-  char input;
-  bool valid = true;
-  for(int i = 0; i < 4;i++){
-    input = keypad.waitForKey();
-    Serial.println(input);
-    if(input != passw[i]){
-      valid = false;
-    }
-  }
-   if(!valid){
-    digitalWrite(redLed, HIGH);
-    delay(500);
-    digitalWrite(redLed, LOW);
-    delay(500);
-    digitalWrite(redLed, HIGH);
-    delay(500);
-    digitalWrite(redLed, LOW);
-    delay(500);
-    digitalWrite(redLed, HIGH);
-    delay(500);
-    digitalWrite(redLed, LOW);
-    delay(500);
-    digitalWrite(redLed, HIGH);
-  } else {
-    digitalWrite(greenLed, LOW);
-    digitalWrite(redLed, LOW);
+  if(checkPass() || detectorLuz()){
+    digitalWrite(ledVerde, LOW);
     delay(1000);
-    digitalWrite(greenLed, HIGH);
+    digitalWrite(ledRojo, HIGH);
 
-    
-    while (true) { 
-      int lightValue = analogRead(A5);
-      int ultra = ultrasound();
-      Serial.println(lightValue);
-      Serial.println(ultra);
-      if(lightValue < 200 && !ultra){
-        digitalWrite(greenLed, LOW);
-        digitalWrite(redLed, HIGH);
-        break;
-      } else if(lightValue > 200 && !ultra){
-        delay(5000);
-        break;
-      }     
+    milis = millis();
+
+    while(resultTime < 5000){
+      if(!detectorUltrasonidosYLuz() && keypad.getKey() == 'C'){      
+          break;
+      }
+      milis5000 = millis();
+      resultTime = milis5000-milis;
+      //detectorUltrasonidosYLuz();
+      
     }
-
-    digitalWrite(greenLed, LOW);
-    digitalWrite(redLed, HIGH);
+    
+    resultTime = 0;
+    
+    digitalWrite(ledVerde, HIGH);
+    digitalWrite(ledRojo, LOW);
   }
-  return valid;
 }
 
-// Check if ultrasound is detecting smth at 10 cntm or less
-bool ultrasound(){
+bool checkPass(){
+  char digit;
+  bool correct = true;
+  for(int i = 0; i < longitudPass; i++){
+    digit = keypad.waitForKey();
+    if(digit != password[i]){
+      correct = false;
+    }
+  }
+
+  if(!correct){
+    digitalWrite(ledVerde, HIGH);
+    delay(500);
+    digitalWrite(ledVerde, LOW);
+    delay(500);
+    digitalWrite(ledVerde, HIGH);
+    delay(500);
+    digitalWrite(ledVerde, LOW);
+    delay(500);
+    digitalWrite(ledVerde, HIGH);
+    delay(500);
+    digitalWrite(ledVerde, LOW);
+    delay(500);
+    digitalWrite(ledVerde, HIGH);
+    return correct;
+  }
+  return correct;
+}
+
+bool detectorUltrasonidosYLuz(){
   int cm = ping(trig,echo);
-  Serial.print(cm);
-  if(cm <= 10){
-    millisTime = millis();
-    result = 0;
+  if(cm <= 10 || analogRead(fotorresistor) < 200){
+    milis = millis();
+    resultTime = 0;
     return true;
   }
   return false;
 }
 
-int ping(int trig, int echo) {
-  long timeBetween, cm;
+int ping(int TriggerPin, int EchoPin) {
+  long duration, distanceCm;
   
-  digitalWrite(trig, LOW);  
+  digitalWrite(TriggerPin, LOW);  //para generar un pulso limpio ponemos a LOW 4us
   delayMicroseconds(4);
-  digitalWrite(trig, HIGH);  
+  digitalWrite(TriggerPin, HIGH);  //generamos Trigger (disparo) de 10us
   delayMicroseconds(10);
-  digitalWrite(trig, LOW);
+  digitalWrite(TriggerPin, LOW);
+  
+  duration = pulseIn(EchoPin, HIGH);  //medimos el tiempo entre pulsos, en microsegundos
+  
+  distanceCm = duration * 10 / 292/ 2;   //convertimos a distancia, en cm
+  return distanceCm;
+}
 
-  // Time between pulses, in ms
-  timeBetween = pulseIn(echo, HIGH);  
-
-  // Distance in cm
-  cm = timeBetween * 10 / 292/ 2;     
-  return cm;
+bool detectorLuz(){
+  if(analogRead(fotorresistor) < 200){
+    return true;
+  }
+  return false;
 }
